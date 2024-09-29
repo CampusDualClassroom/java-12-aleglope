aa320dbafa25937ff22778a32a7158fba1aa58d8
AGonzL <agonzlopez.11@gmail.com>
2024-09-29 06:39:17 +0200

    Ejercicio 12 completado

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -1,5 +1,63 @@
 package com.campusdual.classroom;
 
 public class Car {
+    public int tachometer;
+    public int speedometer;
+    public int wheelsAngle;
+    public String gear;
+    public static final int MAX_SPEED = 120;
+    private static final int MAX_WHEEL_ANGLE = 45;
+    private static final int MIN_WHEEL_ANGLE = -45;
+    private static final int IDLE_RPM = 700;
 
+    public Car() {
+        this.tachometer = 0;
+        this.speedometer = 0;
+        this.wheelsAngle = 0;
+        this.gear = "N";
+    }
+
+    public boolean isTachometerGreaterThanZero() {
+        return tachometer > 0;
+    }
+
+    public boolean isTachometerEqualToZero() {
+        return tachometer == 0;
+    }
+
+    public void start() {
+        if (tachometer == 0) {
+            tachometer = IDLE_RPM;
+        }
+    }
+
+    public void stop() {
+        if (speedometer == 0) {
+            tachometer = 0;
+        }
+    }
+
+    public void accelerate() {
+        if (tachometer > 0) {
+            speedometer = Math.min(speedometer + 5, MAX_SPEED);
+            tachometer = IDLE_RPM + (speedometer * 10);
+        }
+    }
+
+    public void brake() {
+        if (speedometer > 0) {
+            speedometer = Math.max(speedometer - 5, 0);
+            tachometer = IDLE_RPM + (speedometer * 10);
+        }
+    }
+
+    public void turnAngleOfWheels(int angle) {
+        wheelsAngle = Math.max(MIN_WHEEL_ANGLE, Math.min(MAX_WHEEL_ANGLE, angle));
+    }
+
+    public void setReverse(boolean reverse) {
+        if (speedometer == 0) {
+            gear = reverse ? "R" : "D";
+        }
+    }
 }
--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,81 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car car = new Car();
 
+        // Verificar si el tacómetro es cero
+        System.out.println("Is tachometer zero? " + car.isTachometerEqualToZero());
+
+        // Encender el coche
+        car.start();
+        System.out.println("Car started. Tachometer: " + car.tachometer);
+
+        // Intentar encender el coche cuando ya está encendido
+        car.start();
+        System.out.println("Tried to start again. Tachometer: " + car.tachometer);
+
+        // Apagar el coche
+        car.stop();
+        System.out.println("Car stopped. Tachometer: " + car.tachometer);
+
+        // Encender el coche de nuevo
+        car.start();
+        System.out.println("Car started again. Tachometer: " + car.tachometer);
+
+        // Acelerar el coche
+        for (int i = 0; i < 10; i++) {
+            car.accelerate();
+        }
+        System.out.println("Accelerated. Speed: " + car.speedometer);
+
+        // Intentar superar la velocidad máxima
+        for (int i = 0; i < 20; i++) {
+            car.accelerate();
+        }
+        System.out.println("Tried to exceed max speed. Speed: " + car.speedometer);
+
+        // Frenar
+        for (int i = 0; i < 6; i++) {
+            car.brake();
+        }
+        System.out.println("Braked. Speed: " + car.speedometer);
+
+        // Intentar frenar hasta un valor negativo
+        for (int i = 0; i < 20; i++) {
+            car.brake();
+        }
+        System.out.println("Tried to brake to negative. Speed: " + car.speedometer);
+
+        // Girar el volante 20 grados
+        car.turnAngleOfWheels(20);
+        System.out.println("Turned wheels. Angle: " + car.wheelsAngle);
+
+        // Intentar girar el volante más de 45 grados
+        car.turnAngleOfWheels(50);
+        System.out.println("Tried to turn wheels over 45 degrees. Angle: " + car.wheelsAngle);
+
+        // Intentar apagar el coche mientras está en movimiento
+        car.accelerate();
+        car.stop();
+        System.out.println("Tried to stop while moving. Tachometer: " + car.tachometer);
+
+        // Frenar hasta detenerse
+        while (car.speedometer > 0) {
+            car.brake();
+        }
+        System.out.println("Stopped. Speed: " + car.speedometer);
+
+        // Poner marcha atrás
+        car.setReverse(true);
+        System.out.println("Set to reverse. Gear: " + car.gear);
+
+        // Intentar poner marcha atrás mientras el coche está en movimiento
+        car.accelerate();
+        car.setReverse(true);
+        System.out.println("Tried to set reverse while moving. Gear: " + car.gear);
+
+        // Apagar el coche
+        car.stop();
+        System.out.println("Car stopped. Tachometer: " + car.tachometer);
     }
-
 }
